9268c0cafa8a9da0483f32e8d3bcc206d229d118
TheAIBot <andreasuranus@hotmail.com>
2021-02-08 14:03:00 +0100

    Merge input/output change events into one event

---
+++ ChiselDebuggerWebUI/ChiselDebuggerWebUI/Code/IOPositions.cs
@@ -0,0 +1,20 @@
+﻿using ChiselDebug;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChiselDebuggerWebUI.Code
+{
+    public readonly struct IOPositions
+    {
+        public readonly List<Positioned<Input>> Inputs;
+        public readonly List<Positioned<Output>> Outputs;
+
+        public IOPositions(List<Positioned<Input>> inputs, List<Positioned<Output>> outputs)
+        {
+            this.Inputs = inputs;
+            this.Outputs = outputs;
+        }
+    }
+}
