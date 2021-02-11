5a2308df449b5ef78ad77bfe2aa8d60a2c363022
TheAIBot <andreasuranus@hotmail.com>
2021-02-11 12:38:12 +0100

    added float variants for Point and Rectangle

---
+++ ChiselDebug/ChiselDebug/PointF.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChiselDebug
+{
+    public struct PointF
+    {
+        public float X;
+        public float Y;
+
+        public PointF(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PointF point &&
+                   X == point.X &&
+                   Y == point.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return $"[{X}, {Y}]";
+        }
+
+        public static PointF operator +(PointF a, PointF b)
+        {
+            return new PointF(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static PointF operator -(PointF a, PointF b)
+        {
+            return new PointF(a.X - b.X, a.Y - b.Y);
+        }
+        public static PointF operator -(PointF a)
+        {
+            return new PointF(-a.X, -a.Y);
+        }
+
+        public static PointF operator *(PointF a, float b)
+        {
+            return new PointF(a.X * b, a.Y * b);
+        }
+
+        public static bool operator ==(PointF left, PointF right)
+        {
+            return left.X == right.X && left.Y == right.Y;
+        }
+
+        public static bool operator !=(PointF left, PointF right)
+        {
+            return !(left == right);
+        }
+
+        public static PointF Max(PointF a, PointF b)
+        {
+            return new PointF(Math.Max(a.X, b.X), Math.Max(a.Y, b.Y));
+        }
+
+        public static PointF Min(PointF a, PointF b)
+        {
+            return new PointF(Math.Min(a.X, b.X), Math.Min(a.Y, b.Y));
+        }
+
+        public PointF Abs()
+        {
+            return new PointF(Math.Abs(X), Math.Abs(Y));
+        }
+    }
+}
---
+++ ChiselDebug/ChiselDebug/RectangleF.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChiselDebug
+{
+    public readonly struct RectangleF
+    {
+        public readonly PointF Pos;
+        public readonly float Width;
+        public readonly float Height;
+
+        public float LeftX => Pos.X;
+        public float TopY => Pos.Y;
+
+        public float RightX => Pos.X + Width;
+
+        public float BottomY => Pos.Y + Height;
+
+        public PointF Size => new PointF(Width, Height);
+
+
+        public RectangleF(float x, float y, float width, float height) : this(new PointF(x, y), width, height)
+        { }
+
+        public RectangleF(PointF pos, float width, float height)
+        {
+            this.Pos = pos;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public RectangleF(PointF pos, PointF size) : this(pos, size.X, size.Y)
+        { }
+
+        public bool Within(PointF pos)
+        {
+            return LeftX <= pos.X && pos.X <= RightX &&
+                   TopY <= pos.Y && pos.Y <= BottomY;
+        }
+
+        public bool WithinButExcludeRectEdge(PointF pos)
+        {
+            return LeftX < pos.X && pos.X < RightX &&
+                   TopY < pos.Y && pos.Y < BottomY;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is RectangleF rectangle &&
+                   Pos == rectangle.Pos &&
+                   Width == rectangle.Width &&
+                   Height == rectangle.Height;
+        }
+
+        public static bool operator ==(RectangleF left, RectangleF right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(RectangleF left, RectangleF right)
+        {
+            return !(left == right);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Pos, Width, Height);
+        }
+    }
+}
