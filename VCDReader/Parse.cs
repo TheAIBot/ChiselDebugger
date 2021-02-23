471236d6780335a64685ca24513aacf79ead53b0
TheAIBot <andreasuranus@hotmail.com>
2021-02-23 16:40:27 +0100

    can now parse from string, filepath or stream

--- VCDReader/Parse.cs
+++ VCDReader/Parse.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,14 +15,21 @@ namespace VCDReader
 {
     public static class Parse
     {
-        //public static VCD FromFile()
-        //{
-
-        //}
+        public static VCD FromFile(string filepath)
+        {
+            using var fileStream = File.OpenText(filepath);
+            return FromStream(fileStream);
+        }
 
         public static VCD FromString(string vcdString)
         {
-            ICharStream charStream = new AntlrInputStream(vcdString);
+            using var stringStream = new StringReader(vcdString);
+            return FromStream(stringStream);
+        }
+
+        public static VCD FromStream(TextReader stream)
+        {
+            ICharStream charStream = new AntlrInputStream(stream);
             VCDLexer lexer = new VCDLexer(charStream);
             //lexer.TokenFactory = new CommonTokenFactory(true);
             //ITokenStream tokenStream = new UnbufferedTokenStream(lexer);
