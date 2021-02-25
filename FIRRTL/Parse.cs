cfa0781c4ed2dd24bf0fa6d6afb5fec2ca8bf269
TheAIBot <andreasuranus@hotmail.com>
2021-02-25 12:32:42 +0100

    can now parse firrtl from string, stream or file

--- FIRRTL/Parse.cs
+++ FIRRTL/Parse.cs
@@ -2,6 +2,7 @@
 using FIRRTL.Parsing;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace FIRRTL
@@ -10,7 +11,19 @@ namespace FIRRTL
     {
         public static Circuit FromString(string firrtl)
         {
-            ICharStream charStream = new AntlrInputStream(firrtl);
+            using TextReader stream = new StringReader(firrtl);
+            return FromStream(stream);
+        }
+
+        public static Circuit FromFile(string filepath)
+        {
+            using TextReader stream = File.OpenText(filepath);
+            return FromStream(stream);
+        }
+
+        public static Circuit FromStream(TextReader stream)
+        {
+            ICharStream charStream = new AntlrInputStream(stream);
             FIRRTLLexer lexer = new FIRRTLLexer(charStream, true);
             ITokenStream tokenStream = new CommonTokenStream(lexer);
             FIRRTLParser parser = new FIRRTLParser(tokenStream);
