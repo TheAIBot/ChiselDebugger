ba5bc86f759e2fbeae5feaf8f72dacc1803f0f83
TheAIBot <andreasuranus@hotmail.com>
2021-02-25 15:56:46 +0100

    Mux and module now uses the same code to evenly place io vertically

---
+++ ChiselDebuggerWebUI/ChiselDebuggerWebUI/Code/IOPositionCalc.cs
@@ -0,0 +1,45 @@
+﻿using ChiselDebug;
+using ChiselDebug.GraphFIR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChiselDebuggerWebUI.Code
+{
+    internal static class IOPositionCalc
+    {
+        private const int MaxSpaceBetweenIO = 40;
+
+        internal static List<DirectedIO> EvenVertical<T>(int height, List<T> io, int fixedX, int startY) where T : FIRIO
+        {
+            int usableHeight = height - startY * 2;
+            int spaceBetweenIO;
+            if (io.Count <= 1)
+            {
+                startY += usableHeight / 2;
+                spaceBetweenIO = 0;
+            }
+            else
+            {
+                int spacersNeeded = io.Count - 1;
+                int possibleSpaceBetweenIO = usableHeight / spacersNeeded;
+                spaceBetweenIO = Math.Min(MaxSpaceBetweenIO, possibleSpaceBetweenIO);
+                int usedSpace = spaceBetweenIO * spacersNeeded;
+
+                startY += (usableHeight - usedSpace) / 2;
+            }
+
+            List<DirectedIO> posIO = new List<DirectedIO>();
+            for (int i = 0; i < io.Count; i++)
+            {
+                int y = startY + spaceBetweenIO * i;
+
+                Point pos = new Point(fixedX, y);
+                posIO.Add(new DirectedIO(io[i], pos, MoveDirs.Right));
+            }
+
+            return posIO;
+        }
+    }
+}
