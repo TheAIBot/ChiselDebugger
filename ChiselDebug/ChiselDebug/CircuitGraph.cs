cfd31c17c840e09d4d49746c6da52fd88339cb9f
TheAIBot <andreasuranus@hotmail.com>
2021-03-01 18:28:58 +0100

    set state in graph from circuit state

--- ChiselDebug/ChiselDebug/CircuitGraph.cs
+++ ChiselDebug/ChiselDebug/CircuitGraph.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using ChiselDebug.GraphFIR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VCDReader;
 
 namespace ChiselDebug
 {
     public class CircuitGraph
     {
         public readonly string Name;
-        public readonly List<GraphFIR.Module> Modules = new List<GraphFIR.Module>();
+        public readonly List<Module> Modules = new List<Module>();
 
-        public CircuitGraph(string name, List<GraphFIR.Module> modules)
+        public CircuitGraph(string name, List<Module> modules)
         {
             this.Name = name;
             this.Modules = modules;
@@ -18,6 +22,25 @@ namespace ChiselDebug
             foreach (var mod in Modules)
             {
                 mod.InferType();
+            }
+        }
+
+        public void SetState(CircuitState state)
+        {
+            foreach (BinaryVarValue varValue in state.VariableValues.Values)
+            {
+                Scope scope = varValue.Variable.Scopes[0];
+                if (scope.Type == ScopeType.Module)
+                {
+                    Module mod = Modules.First(x => x.Name == scope.Name);
+                    Connection con = mod.GetConnection(varValue.Variable.Scopes.AsSpan().Slice(1), varValue.Variable.Reference);
+
+                    con.Value.SetValue(varValue);
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
             }
         }
     }
