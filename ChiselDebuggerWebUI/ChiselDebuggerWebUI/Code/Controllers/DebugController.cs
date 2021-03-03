2054008b1360b7b283757b8b35cecc84fe50916c
TheAIBot <andreasuranus@hotmail.com>
2021-03-03 14:35:19 +0100

    refactor code that batches work and only executes latest into its own class

--- ChiselDebuggerWebUI/ChiselDebuggerWebUI/Code/Controllers/DebugController.cs
+++ ChiselDebuggerWebUI/ChiselDebuggerWebUI/Code/Controllers/DebugController.cs
@@ -3,12 +3,7 @@ using ChiselDebug.GraphFIR;
 using ChiselDebug.Timeline;
 using ChiselDebuggerWebUI.Components;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace ChiselDebuggerWebUI.Code
 {
@@ -22,63 +17,34 @@ namespace ChiselDebuggerWebUI.Code
         public delegate void ReRenderCircuit();
         public event ReRenderCircuit OnReRender;
 
-        private readonly BlockingCollection<ulong> TimeChanges = new BlockingCollection<ulong>(new ConcurrentQueue<ulong>());
-        private readonly Task TimeChangesWorker;
-        private readonly CancellationTokenSource CancelSource = new CancellationTokenSource();
+        private readonly ExecuteOnlyLatest<ulong> TimeChanger = new ExecuteOnlyLatest<ulong>();
 
         public DebugController(CircuitGraph graph, VCDTimeline timeline)
         {
             this.Graph = graph;
             this.Timeline = timeline;
-            this.TimeChangesWorker = Task.Factory.StartNew(async () =>
+            TimeChanger.Start(time =>
             {
-                try
+                List<Connection> changedConnections = Graph.SetState(Timeline.GetStateAtTime(time));
+
+                //Only rerender the uiNodes that are connected to a connection
+                //that changed value. UiNodes are not rerendered here, but they
+                //are being allowed to rerender here.
+                foreach (var connection in changedConnections)
                 {
-                    var token = CancelSource.Token;
-
-                    while (!token.IsCancellationRequested)
+                    if (ConToUINode.TryGetValue(connection, out var uiNodes))
                     {
-                        ulong time = TimeChanges.Take(token);
-
-                        //Many time changes may be queued up and the new circuit state
-                        //should be set to the latest one added to the queue so the 
-                        //shown circuit state is as up to date as possible
-                        if (TimeChanges.Count > 0)
+                        foreach (var uiNode in uiNodes)
                         {
-                            time = TimeChanges.Take(TimeChanges.Count).Last();
+                            uiNode.PrepareForRender();
                         }
-
-                        List<Connection> changedConnections = Graph.SetState(Timeline.GetStateAtTime(time));
-
-                        //Only rerender the uiNodes that are connected to a connection
-                        //that changed value. UiNodes are not rerendered here, but they
-                        //are being allowed to rerender here.
-                        foreach (var connection in changedConnections)
-                        {
-                            if (ConToUINode.TryGetValue(connection, out var uiNodes))
-                            {
-                                foreach (var uiNode in uiNodes)
-                                {
-                                    uiNode.PrepareForRender();
-                                }
-                            }
-                        }
-
-                        //Ask ui to render again so the allowed uiNodes
-                        //can be rerendered.
-                        OnReRender?.Invoke();
-
-                        //Limit how often it can update the circuit state so it
-                        //doesn't end up rerendering as fast as possible.
-                        await Task.Delay(10);
                     }
                 }
-                catch (OperationCanceledException e) { }
-                catch (Exception e)
-                {
-                    Debug.WriteLine(e.Message + Environment.NewLine + e.StackTrace);
-                }
-            }, TaskCreationOptions.LongRunning);
+
+                //Ask ui to render again so the allowed uiNodes
+                //can be rerendered.
+                OnReRender?.Invoke();
+            });
         }
 
         public void AddUINode(IFIRUINode node, List<Connection> connections)
@@ -105,13 +71,12 @@ namespace ChiselDebuggerWebUI.Code
 
         public void SetCircuitState(ulong time)
         {
-            TimeChanges.Add(time);
+            TimeChanger.AddWork(time);
         }
 
         public void Dispose()
         {
-            CancelSource.Cancel();
-            CancelSource.Dispose();
+            TimeChanger.Dispose();
         }
     }
 }
