f3d717e7fccdb654de6800d076f95a829d48dcf6
TheAIBot <andreasuranus@hotmail.com>
2021-03-03 14:10:36 +0100

    can now zoom in and out on the circuit

---
+++ ChiselDebuggerWebUI/ChiselDebuggerWebUI/Code/JSEvents.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChiselDebuggerWebUI.Code
+{
+    public static class JSEvents
+    {
+        public delegate void ScrollHandler(int scrollDelta);
+        public static event ScrollHandler OnWindowScroll;
+
+        [JSInvokable]
+        public static void ScrollEventAsync(int scrollDelta)
+        {
+            OnWindowScroll?.Invoke(scrollDelta);
+        }
+    }
+}
--- ChiselDebuggerWebUI/ChiselDebuggerWebUI/wwwroot/scripts/utilities.js
+++ ChiselDebuggerWebUI/ChiselDebuggerWebUI/wwwroot/scripts/utilities.js
@@ -11,3 +11,29 @@ JSUtils.getElementSize = function(element) {
     const elemRect = element.getBoundingClientRect();
     return new ElemWH(elemRect.width, elemRect.height);
 };
+
+
+let isScrollQueued = false;
+let scrollDelta = 0;
+
+function FireScrollEvent() {
+    isScrollQueued = false;
+
+    let delta = scrollDelta;
+    scrollDelta = 0;
+    DotNet.invokeMethodAsync("ChiselDebuggerWebUI", "ScrollEventAsync", delta);
+}
+
+document.addEventListener("wheel", function (e) {
+    if (e.deltaY > 0) {
+        scrollDelta++;
+    }
+    else {
+        scrollDelta--;
+    }
+
+    if (!isScrollQueued) {
+        isScrollQueued = true;
+        window.requestAnimationFrame(FireScrollEvent);
+    }
+}, true);
