75b387e35efdc592f0c2a59a71e5cc873683afbd
TheAIBot <andreasuranus@hotmail.com>
2021-03-18 18:49:32 +0100

    Test if register can contain vector

--- ChiselDebuggerWebUI/ChiselDebugTests/RegisterTests.cs
+++ ChiselDebuggerWebUI/ChiselDebugTests/RegisterTests.cs
@@ -40,5 +40,22 @@ circuit ModA :
 
             TestTools.VerifyCanCreateGraph(firrtl);
         }
+
+        [TestMethod]
+        public void RegVector()
+        {
+            string firrtl = @"
+circuit ModA : 
+  module ModA : 
+    input clock : Clock
+    input reset : UInt<1>
+    output io : {flip din : UInt<8>[5], dout : UInt<8>[5]}
+    
+    reg rr : UInt<8>[5], clock
+    rr <= io.din
+    io.dout <= rr";
+
+            TestTools.VerifyCanCreateGraph(firrtl);
+        }
     }
 }
