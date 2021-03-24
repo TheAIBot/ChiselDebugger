c65a332d93b8336b6c0d21c1dd859e356c94da30
TheAIBot <andreasuranus@hotmail.com>
2021-03-24 13:19:06 +0100

    dfsg

--- ChiselDebug/ChiselDebug/Routing/ScorePath.cs
+++ ChiselDebug/ChiselDebug/Routing/ScorePath.cs
@@ -13,7 +13,7 @@
             this.DirFrom = fromDir;
         }
 
-        public ScorePath Move(MoveDirs dir, bool onEnemyWire, bool onWireCorner, bool isTurningOnEnemyWire)
+        public ScorePath Move(MoveDirs revDir, bool onEnemyWire, bool onWireCorner, bool isTurningOnEnemyWire)
         {
             //If the direction we are moving in is not the
             //opposite of the direction we came from,
@@ -21,25 +21,26 @@
             //the path from this point. If we didn't come
             //from any direction then we must've started
             //the path from here.
-            bool isTurning = DirFrom != dir.Reverse() && DirFrom != MoveDirs.None;
+            bool isTurning = DirFrom != revDir && DirFrom != MoveDirs.None;
             int turns = TurnsTaken;
             turns += isTurning ? 1 : 0;
             turns += onEnemyWire ? 1 : 0;
             turns += onWireCorner ? 500 : 0;
             turns += isTurningOnEnemyWire ? 50 : 0;
 
-            return new ScorePath(TraveledDist + 1, turns, dir.Reverse());
+            return new ScorePath(TraveledDist + 1, turns, revDir);
         }
 
         public bool IsBetterScoreThan(ScorePath score)
         {
-            if (TraveledDist < score.TraveledDist)
-            {
-                return true;
-            }
+            return GetTotalScore() < score.GetTotalScore();
+            //if (TraveledDist < score.TraveledDist)
+            //{
+            //    return true;
+            //}
 
-            return TraveledDist == score.TraveledDist &&
-                   TurnsTaken < score.TurnsTaken;
+            //return TraveledDist == score.TraveledDist &&
+            //       TurnsTaken < score.TurnsTaken;
         }
 
         public int GetTotalScore()
