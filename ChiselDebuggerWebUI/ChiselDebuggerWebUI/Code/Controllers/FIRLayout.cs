bbd06e0a1c032ffc32b72a55c44086f015b877c7
TheAIBot <andreasuranus@hotmail.com>
2021-04-14 11:36:33 +0200

    more classes to their own files

---
+++ ChiselDebuggerWebUI/ChiselDebuggerWebUI/Code/Controllers/FIRLayout.cs
@@ -0,0 +1,25 @@
+﻿using ChiselDebuggerWebUI.Components;
+using System.Collections.Generic;
+
+namespace ChiselDebuggerWebUI.Code
+{
+    public abstract class FIRLayout
+    {
+        protected readonly List<IFIRUINode> UINodes = new List<IFIRUINode>();
+
+        public void AddUINode(IFIRUINode uiNode)
+        {
+            UINodes.Add(uiNode);
+        }
+
+        public void PrepareToRerenderLayout()
+        {
+            foreach (var uiNode in UINodes)
+            {
+                uiNode.PrepareForRender();
+            }
+        }
+
+        public abstract void UpdateComponentInfo(FIRComponentUpdate updateData);
+    }
+}
