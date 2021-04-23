1de7a52e217acc5536ac79263fa52433102590a3
TheAIBot <andreasuranus@hotmail.com>
2021-04-24 00:01:48 +0200

    project used to benchmark and profile project

---
+++ Benchmarker/Program.cs
@@ -0,0 +1,34 @@
+﻿using ChiselDebug;
+using FIRRTL;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Benchmarker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circuit circuddit = FIRRTL.Parse.FromFile(Path.Combine(dir, "TileTester_4.lo.fir"));
+            //ConcurrentBag<CircuitGraph> graphs = new ConcurrentBag<CircuitGraph>();
+            //Parallel.For(0, 8, x =>
+            //    {
+            //        graphs.Add(CircuitToGraph.GetAsGraph(circuddit));
+            //    });
+            //foreach (var graph in graphs)
+            //{
+            //    Console.WriteLine(graph.Name);
+            //}
+
+            CircuitGraph lol;
+            for (int i = 0; i < 10; i++)
+            {
+                lol = CircuitToGraph.GetAsGraph(circuddit);
+            }
+
+
+        }
+    }
+}
