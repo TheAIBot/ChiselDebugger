3b12589048c0b310406824e1879f320536535229
TheAIBot <andreasuranus@hotmail.com>
2021-04-24 18:14:04 +0200

    dgg

--- ChiselDebuggerWebUI/ChiselDebuggerWebUI/Code/Controllers/DebugController.cs
+++ ChiselDebuggerWebUI/ChiselDebuggerWebUI/Code/Controllers/DebugController.cs
@@ -1,4 +1,5 @@
 ﻿using ChiselDebug;
+using ChiselDebug.CombGraph;
 using ChiselDebug.GraphFIR;
 using ChiselDebug.GraphFIR.IO;
 using ChiselDebug.Routing;
@@ -22,6 +23,8 @@ namespace ChiselDebuggerWebUI.Code
         private readonly BroadcastBlock<Action> TimeChanger = null;
         private readonly PlacementTemplator PlacementTemplates = new PlacementTemplator();
         private readonly RouteTemplator RouteTemplates = new RouteTemplator();
+        private readonly Dictionary<Output, string> ConToColor = new Dictionary<Output, string>();
+        private readonly HashSet<Output> ConstCons = new HashSet<Output>();
         public Point CircuitSize { get; private set; } = Point.Zero;
 
         public DebugController(CircuitGraph graph, VCD vcd)
@@ -33,6 +36,82 @@ namespace ChiselDebuggerWebUI.Code
                 this.Timeline = new VCDTimeline(vcd);
                 this.TimeChanger = new BroadcastBlock<Action>(x => x);
                 WorkLimiter.LinkSource(TimeChanger);
+
+                SetCircuitState(Timeline.TimeInterval.StartInclusive);
+            }
+
+            Dictionary<Output, CombComputeNode> conToCombNode = new Dictionary<Output, CombComputeNode>();
+            CombComputeNode[] combNodes = graph.ComputeGraph.GetValueChangingNodes();
+            foreach (var combNode in combNodes)
+            {
+                foreach (var combCon in combNode.GetResponsibleConnections())
+                {
+                    //if (conToCombNode.ContainsKey(combCon))
+                    //{
+
+                    //}
+                    //conToCombNode[combCon] = combNode;
+                    conToCombNode.Add(combCon, combNode);
+                }
+            }
+
+            string[] colors = new string[]
+            {
+                "red",
+                "green",
+                "blue",
+                "orange",
+                "purple",
+                //"black",
+                "brown",
+                "teal",
+                //"maroon",
+                "magenta",
+                "grey"
+            };
+            int colorIndex = 0;
+            Dictionary<CombComputeNode, string> combToColor = new Dictionary<CombComputeNode, string>();
+            foreach (var combNode in combNodes)
+            {
+                combToColor.Add(combNode, colors[colorIndex]);
+                colorIndex = (colorIndex + 1) % colors.Length;
+            }
+
+            foreach (var conCombNode in conToCombNode)
+            {
+                ConToColor.Add(conCombNode.Key, combToColor[conCombNode.Value]);
+            }
+
+            foreach (var combNode in graph.ComputeGraph.GetConstNodes())
+            {
+                foreach (var con in combNode.GetResponsibleConnections())
+                {
+                    ConstCons.Add(con);
+                }
+            }
+        }
+
+        public bool IsConnectionConst(Output con)
+        {
+            lock (ConToColor)
+            {
+                return ConstCons.Contains(con);
+            }
+        }
+
+        public string GetConnectionColor(Output con)
+        {
+            lock (ConToColor)
+            {
+                if (ConstCons.Contains(con))
+                {
+                    return "black";
+                }
+                if (!ConToColor.ContainsKey(con))
+                {
+                    return "black";
+                }
+                return ConToColor[con];
             }
         }
 
