2ec001f1598c3d4ec3d6f6a4a862d08fd2da3cd2
TheAIBot <andreasuranus@hotmail.com>
2021-05-13 14:24:57 +0200

    benchmarker can now easily benchmark vcdtimeline and compute graph

--- Benchmarker/Program.cs
+++ Benchmarker/Program.cs
@@ -1,9 +1,13 @@
 ﻿using ChiselDebug;
+using ChiselDebug.GraphFIR.IO;
+using ChiselDebug.Timeline;
 using FIRRTL;
 using System;
 using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
+using VCDReader;
 
 namespace Benchmarker
 {
@@ -11,25 +15,88 @@ namespace Benchmarker
     {
         static void Main(string[] args)
         {
-            const string dir = "";
-            Circuit circuddit = FIRRTL.Parse.FromFile(Path.Combine(dir, "TileTester_4.lo.fir"));
-            //ConcurrentBag<CircuitGraph> graphs = new ConcurrentBag<CircuitGraph>();
-            //Parallel.For(0, 8, x =>
-            //    {
-            //        graphs.Add(CircuitToGraph.GetAsGraph(circuddit));
-            //    });
-            //foreach (var graph in graphs)
-            //{
-            //    Console.WriteLine(graph.Name);
-            //}
+            const string TestDir = @"...";
+            Stopwatch timer = new Stopwatch();
 
-            CircuitGraph lol;
-            for (int i = 0; i < 10; i++)
+            if (true)
             {
-                lol = CircuitToGraph.GetAsGraph(circuddit);
+                timer.Start();
+                VerifyComputeGraph("TileTester_4", "lo.fir", true, TestDir);
+                timer.Stop();
+            }
+            else
+            {
+                using VCD vcd = VCDReader.Parse.FromFile(Path.Combine(TestDir, "TileTester_6.vcd"));
+                //using VCD vcd = VCDReader.Parse.FromFile(Path.Combine(TestDir, "TileTester_8.vcd"));
+                //using VCD vcd = VCDReader.Parse.FromFile(Path.Combine(TestDir, "TileTester_4.vcd"));
+                //using VCD vcd = VCDReader.Parse.FromFile(Path.Combine(TestDir, "TileTester_5.vcd"));
+
+                timer.Start();
+                new VCDTimeline(vcd);
+                timer.Stop();
             }
 
+            //Console.WriteLine((timer.ElapsedMilliseconds / 1000.0f).ToString("N2"));
 
+
+        }
+
+        internal static void VerifyComputeGraph(string moduleName, string extension, bool isVerilogVCD, string modulePath)
+        {
+            CircuitGraph graph = VerifyMakeGraph(moduleName, extension, modulePath);
+            VCDTimeline timeline = MakeTimeline(moduleName, modulePath);
+
+            VerifyCircuitState(graph, timeline, isVerilogVCD);
+        }
+
+        private static VCDTimeline MakeTimeline(string moduleName, string modulePath)
+        {
+            using VCD vcd = LoadVCD(moduleName, modulePath);
+            return new VCDTimeline(vcd);
+        }
+
+        private static VCD LoadVCD(string moduleName, string modulePath)
+        {
+            return VCDReader.Parse.FromFile($"{modulePath}/{moduleName}.vcd");
+        }
+
+        internal static CircuitGraph VerifyMakeGraph(string moduleName, string extension, string modulePath)
+        {
+            CircuitGraph lowFirGraph = null;
+            if (extension == "fir")
+            {
+                string loFirPath = Path.Combine(modulePath, $"{moduleName}.lo.fir");
+                lowFirGraph = VerifyCanCreateGraphFromFile(loFirPath);
+            }
+
+            string firPath = Path.Combine(modulePath, $"{moduleName}.{extension}");
+            return VerifyCanCreateGraphFromFile(firPath, lowFirGraph);
+        }
+
+        private static CircuitGraph VerifyCanCreateGraphFromFile(string firrtlPath, CircuitGraph lowFirGraph = null)
+        {
+            using FileStream fileStream = new FileStream(firrtlPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using TextReader textStream = new StreamReader(fileStream);
+
+            Circuit circuit = FIRRTL.Parse.FromStream(textStream);
+            CircuitGraph graph = CircuitToGraph.GetAsGraph(circuit, lowFirGraph);
+
+            return graph;
+        }
+
+        internal static void VerifyCircuitState(CircuitGraph graph, VCDTimeline timeline, bool isVerilogVCD)
+        {
+            graph.SetState(timeline.GetStateAtTime(timeline.TimeInterval.StartInclusive), isVerilogVCD);
+
+            foreach (var state in timeline.GetAllDistinctStates())
+            {
+                if (state.Time == timeline.TimeInterval.InclusiveEnd())
+                {
+                    break;
+                }
+                graph.SetState(state, isVerilogVCD);
+                graph.ComputeRemainingGraphFast();
+            }
         }
     }
 }
