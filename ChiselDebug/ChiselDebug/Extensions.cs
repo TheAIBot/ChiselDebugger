b4a45fae7297d63dffeb2bcc0362d4b0e80910cb
TheAIBot <andreasuranus@hotmail.com>
2021-07-25 00:42:43 +0200

    remove unused code

--- ChiselDebug/ChiselDebug/Extensions.cs
+++ ChiselDebug/ChiselDebug/Extensions.cs
@@ -8,38 +8,6 @@ namespace ChiselDebug
 {
     public static class Extensions
     {
-        public static string ToSignedBinaryString(this BigInteger value, FIRRTL.GroundType type)
-        {
-            string binary = value.ToBinaryString();
-            binary = binary.Substring(0, Math.Min(binary.Length, type.Width));
-
-            if (type is FIRRTL.SIntType)
-            {
-                return binary.PadLeft(type.Width, binary.Last());
-            }
-            else if (type is FIRRTL.UIntType)
-            {
-                return binary.PadLeft(type.Width, '0');
-            }
-            else
-            {
-                throw new Exception();
-            }
-        }
-
-        public static string ToBinaryString(this BigInteger value)
-        {
-            StringBuilder sBuilder = new StringBuilder();
-
-            byte[] valueBytes = value.ToByteArray();
-            foreach (var vByte in valueBytes)
-            {
-                sBuilder.Append(Convert.ToString(vByte, 2));
-            }
-
-            return sBuilder.ToString();
-        }
-
         public static bool IsVertical(this MoveDirs dir)
         {
             return dir.HasFlag(MoveDirs.Up) || dir.HasFlag(MoveDirs.Down);
