7d751af01c210b66adc76a08407135b2e3bf883f
TheAIBot <andreasuranus@hotmail.com>
2021-12-20 19:03:28 +0100

    refactor converting scalar connections intop aggregate connections

--- ChiselDebug/ChiselDebug/ConnectionsHandler.cs
+++ ChiselDebug/ChiselDebug/ConnectionsHandler.cs
@@ -42,156 +42,173 @@ namespace ChiselDebug
         {
             lock (this)
             {
-                Dictionary<FIRRTLNode, Point> nodePoses = new Dictionary<FIRRTLNode, Point>();
-                foreach (var nodePos in placements.NodePositions)
-                {
-                    nodePoses.Add(nodePos.Value, nodePos.Position);
-                }
+                Dictionary<FIRRTLNode, Point> nodePoses = placements.NodePositions.ToDictionary(x => x.Value, x => x.Position);
                 nodePoses.Add(Mod, Point.Zero);
 
                 List<(IOInfo start, IOInfo end)> lines = new List<(IOInfo start, IOInfo end)>();
+                lines.AddRange(MakeAggregateLines(nodePoses, out var disallowedConnections));
+                lines.AddRange(MakeScalarLines(nodePoses, disallowedConnections));
 
-                Dictionary<Output, HashSet<Input>> disallowedConnections = new Dictionary<Output, HashSet<Input>>();
-                HashSet<AggregateIO> alreadyMade = new HashSet<AggregateIO>();
-                List<Output> outputIOs = IOInfos.Keys.OfType<Output>().ToList();
-                foreach (var output in outputIOs)
+                return lines;
+            }
+        }
+
+        private List<(IOInfo start, IOInfo end)> MakeAggregateLines(Dictionary<FIRRTLNode, Point> nodePoses, out Dictionary<Output, HashSet<Input>> disallowedConnections)
+        {
+            disallowedConnections = new Dictionary<Output, HashSet<Input>>();
+            List<(IOInfo start, IOInfo end)> lines = new List<(IOInfo start, IOInfo end)>();
+            HashSet<AggregateIO> alreadyMade = new HashSet<AggregateIO>();
+            List<Output> outputIOs = IOInfos.Keys.OfType<Output>().ToList();
+            foreach (var output in outputIOs)
+            {
+                ScalarIO[] outputParentScalarIO;
+                IOInfo outputInfo;
+                if (!TryCanIOMakeAggregateConnection(alreadyMade, output, out outputParentScalarIO, out outputInfo))
                 {
-                    if (!output.IsPartOfAggregateIO)
+                    continue;
+                }
+
+                foreach (var input in output.GetConnectedInputs())
+                {
+                    ScalarIO[] inputParentScalarIO;
+                    IOInfo inputInfo;
+                    if (!TryCanIOMakeAggregateConnection(alreadyMade, input, out inputParentScalarIO, out inputInfo))
                     {
                         continue;
                     }
 
-
-
-                    var outputParent = output.ParentIO;
-                    if (!IOInfos.ContainsKey(outputParent))
+                    if (outputParentScalarIO.Length != inputParentScalarIO.Length)
                     {
                         continue;
                     }
 
-                    if (alreadyMade.Contains(outputParent))
+                    if (TryGetAllAggregateConnections(outputParentScalarIO, inputParentScalarIO, out var connections))
                     {
-                        continue;
-                    }
-
-                    ScalarIO[] outputParentScalarIO = outputParent.Flatten().ToArray();
-                    if (!outputParentScalarIO.All(x => IOInfos.ContainsKey(x)))
-                    {
-                        continue;
-                    }
-
-                    foreach (var input in output.GetConnectedInputs())
-                    {
-                        if (!input.IsPartOfAggregateIO)
-                        {
-                            continue;
-                        }
-
-                        var inputParent = input.ParentIO;
-                        if (!IOInfos.ContainsKey(inputParent))
-                        {
-                            continue;
-                        }
-
-                        ScalarIO[] inputParentScalarIO = inputParent.Flatten().ToArray();
-                        if (!inputParentScalarIO.All(x => IOInfos.ContainsKey(x)))
-                        {
-                            continue;
-                        }
-
-                        if (outputParentScalarIO.Length != inputParentScalarIO.Length)
-                        {
-                            continue;
-                        }
-
-                        bool isFaliure = false;
-                        List<(Output from, Input to)> connections = new List<(Output from, Input to)>();
-                        for (int i = 0; i < outputParentScalarIO.Length; i++)
-                        {
-                            ScalarIO fromOutputIO = outputParentScalarIO[i];
-                            ScalarIO fromInputIO = inputParentScalarIO[i];
-                            Output outputIO;
-                            Input inputIO;
-                            if (fromOutputIO is Output && fromInputIO is Input)
-                            {
-                                outputIO = (Output)fromOutputIO;
-                                inputIO = (Input)fromInputIO;
-                            }
-                            else if (fromOutputIO is Input && fromInputIO is Output)
-                            {
-                                outputIO = (Output)fromInputIO;
-                                inputIO = (Input)fromOutputIO;
-                            }
-                            else
-                            {
-                                isFaliure = true;
-                                break;
-                            }
-
-                            if (!outputIO.GetConnectedInputs().Any(x => x == inputIO))
-                            {
-                                isFaliure = true;
-                                break;
-                            }
-
-                            connections.Add((outputIO, inputIO));
-                        }
-
-                        if (isFaliure)
-                        {
-                            break;
-                        }
-
+                        // The connections between inputs/outputs are replaced with this
+                        // single aggregate connection and therefore we need to mark the
+                        // scalar connections as not allowed so they won't later be made
                         foreach (var connection in connections)
                         {
                             disallowedConnections.TryAdd(connection.from, new HashSet<Input>());
                             disallowedConnections[connection.from].Add(connection.to);
                         }
 
-                        IOInfo outputInfo = IOInfos[outputParent];
-                        IOInfo inputInfo = IOInfos[inputParent];
-
-                        Point startOffset = nodePoses[outputInfo.Node];
-                        Point endOffset = nodePoses[inputInfo.Node];
-
-                        IOInfo offsetStartIO = new IOInfo(outputInfo.Node, outputInfo.DirIO.WithOffsetPosition(startOffset));
-                        IOInfo offsetEndIO = new IOInfo(inputInfo.Node, inputInfo.DirIO.WithOffsetPosition(endOffset));
-
-                        lines.Add((offsetStartIO, offsetEndIO));
-                        alreadyMade.Add(outputParent);
+                        lines.Add(MakeLine(nodePoses, outputInfo, inputInfo));
                         break;
                     }
                 }
+            }
 
-                HashSet<Input> allAllowed = new HashSet<Input>();
-                foreach (var connection in UsedModuleConnections)
+            return lines;
+        }
+
+        private bool TryCanIOMakeAggregateConnection(HashSet<AggregateIO> alreadyMade, ScalarIO scalar, out ScalarIO[] aggScalarIO, out IOInfo aggIOInfo)
+        {
+            if (!scalar.IsPartOfAggregateIO)
+            {
+                aggScalarIO = null;
+                aggIOInfo = null;
+                return false;
+            }
+
+            var scalarParent = scalar.ParentIO;
+            if (!IOInfos.TryGetValue(scalarParent, out aggIOInfo))
+            {
+                aggScalarIO = null;
+                aggIOInfo = null;
+                return false;
+            }
+
+            if (!alreadyMade.Add(scalarParent))
+            {
+                aggScalarIO = null;
+                aggIOInfo = null;
+                return false;
+            }
+
+            aggScalarIO = scalarParent.Flatten().ToArray();
+            if (!aggScalarIO.All(x => IOInfos.ContainsKey(x)))
+            {
+                aggScalarIO = null;
+                aggIOInfo = null;
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool TryGetAllAggregateConnections(ScalarIO[] aggOutputIO, ScalarIO[] aggInputIO, out List<(Output from, Input to)> connections)
+        {
+            connections = new List<(Output from, Input to)>();
+            for (int i = 0; i < aggOutputIO.Length; i++)
+            {
+                ScalarIO fromOutputIO = aggOutputIO[i];
+                ScalarIO fromInputIO = aggInputIO[i];
+
+                Output outputIO;
+                Input inputIO;
+                if (fromOutputIO is Output && fromInputIO is Input)
                 {
-                    HashSet<Input> notAllowedInputs = disallowedConnections.GetValueOrDefault(connection) ?? allAllowed;
-                    if (IOInfos.TryGetValue(connection, out IOInfo outputInfo))
+                    outputIO = (Output)fromOutputIO;
+                    inputIO = (Input)fromInputIO;
+                }
+                else if (fromOutputIO is Input && fromInputIO is Output)
+                {
+                    outputIO = (Output)fromInputIO;
+                    inputIO = (Input)fromOutputIO;
+                }
+                else
+                {
+                    return false;
+                }
+
+                if (!outputIO.GetConnectedInputs().Any(x => x == inputIO))
+                {
+                    return false;
+                }
+
+                connections.Add((outputIO, inputIO));
+            }
+
+            return true;
+        }
+
+        private List<(IOInfo start, IOInfo end)> MakeScalarLines(Dictionary<FIRRTLNode, Point> nodePoses, Dictionary<Output, HashSet<Input>> disallowedConnections)
+        {
+            List<(IOInfo start, IOInfo end)> lines = new List<(IOInfo start, IOInfo end)>();
+            HashSet<Input> allAllowed = new HashSet<Input>();
+            foreach (var connection in UsedModuleConnections)
+            {
+                HashSet<Input> notAllowedInputs = disallowedConnections.GetValueOrDefault(connection) ?? allAllowed;
+                if (IOInfos.TryGetValue(connection, out IOInfo outputInfo))
+                {
+                    foreach (var input in connection.GetConnectedInputs())
                     {
-                        foreach (var input in connection.GetConnectedInputs())
+                        if (notAllowedInputs.Contains(input))
                         {
-                            if (notAllowedInputs.Contains(input))
-                            {
-                                continue;
-                            }
+                            continue;
+                        }
 
-                            if (IOInfos.TryGetValue(input, out IOInfo inputInfo))
-                            {
-                                Point startOffset = nodePoses[outputInfo.Node];
-                                Point endOffset = nodePoses[inputInfo.Node];
-
-                                IOInfo offsetStartIO = new IOInfo(outputInfo.Node, outputInfo.DirIO.WithOffsetPosition(startOffset));
-                                IOInfo offsetEndIO = new IOInfo(inputInfo.Node, inputInfo.DirIO.WithOffsetPosition(endOffset));
-
-                                lines.Add((offsetStartIO, offsetEndIO));
-                            }
+                        if (IOInfos.TryGetValue(input, out IOInfo inputInfo))
+                        {
+                            lines.Add(MakeLine(nodePoses, outputInfo, inputInfo));
                         }
                     }
                 }
-
-                return lines;
             }
+
+            return lines;
+        }
+
+        private (IOInfo start, IOInfo end) MakeLine(Dictionary<FIRRTLNode, Point> nodePoses, IOInfo outputInfo, IOInfo inputInfo)
+        {
+            Point startOffset = nodePoses[outputInfo.Node];
+            Point endOffset = nodePoses[inputInfo.Node];
+
+            IOInfo offsetStartIO = new IOInfo(outputInfo.Node, outputInfo.DirIO.WithOffsetPosition(startOffset));
+            IOInfo offsetEndIO = new IOInfo(inputInfo.Node, inputInfo.DirIO.WithOffsetPosition(endOffset));
+
+            return (offsetStartIO, offsetEndIO);
         }
     }
 }
