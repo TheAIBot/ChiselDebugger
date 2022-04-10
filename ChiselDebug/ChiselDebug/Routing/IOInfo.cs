5d28405afa6cbe97ea838d4e71c907321e0b85c6
TheAIBot <andreasuranus@hotmail.com>
2022-04-10 12:24:53 +0200

    more refactor

--- ChiselDebug/ChiselDebug/Routing/IOInfo.cs
+++ ChiselDebug/ChiselDebug/Routing/IOInfo.cs
@@ -2,15 +2,5 @@
 
 namespace ChiselDebug.Routing
 {
-    internal sealed class IOInfo
-    {
-        internal readonly FIRRTLNode Node;
-        internal readonly DirectedIO DirIO;
-
-        public IOInfo(FIRRTLNode node, DirectedIO dirIO)
-        {
-            Node = node;
-            DirIO = dirIO;
-        }
-    }
+    internal sealed record IOInfo(FIRRTLNode Node, DirectedIO DirIO);
 }
